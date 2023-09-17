58ef4c155376e7109610ded71835cdbb57120f56
DESKTOP-MIEKMRL\user <mirajulislam5746@gmail.com>
2023-09-17 15:16:02 +0600

    simple test

---
+++ Simple-Maven-Test/src/main/java/com/login/apps/SimpleMavenTestApplication.java
@@ -0,0 +1,13 @@
+package com.login.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMavenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMavenTestApplication.class, args);
+	}
+
+}
---
+++ Simple-Maven-Test/src/test/java/com/login/apps/SimpleMavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMavenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
